549da1d761e4d31f91406f631189eb818457deee
Mate Radnai <mate_radnai@epam.com>
2025-03-11 13:10:23 +0100

    try config options

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['list', { printSteps: true }],
+    // ['json', { outputFile: 'test-results.json' }],
+    // ['html', { outputFolder: 'my-report' }],
+    // ['line'],
+    // ['dot'],
+    // ['blob', { outputFile: `./blob-report/report-${Date.now()}.zip` }],
+    // ['junit', { outputFile: 'results.xml' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,8 +39,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    testIdAttribute: 'data-test'
+    testIdAttribute: 'data-test',
+    headless: false,
   },
+  timeout: 120 * 1000,
 
   /* Configure projects for major browsers */
   projects: [
